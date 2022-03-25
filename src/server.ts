8cf258e976c9fd1b73a5b15ac49c7180984d45aa
Vitor Henrique <vhsouzaeng@gmail.com>
2022-03-25 20:09:18 -0300

    feat: running server

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,17 @@
-console.log('Hello World!!')
-export { }
+import { ApolloServer } from "apollo-server"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
+import { PokemonSchema } from './domains/pokemon/schema/pokemon'
+
+const server = new ApolloServer({
+    typeDefs: [
+        PokemonSchema
+    ],
+    resolvers: [],
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server running at ${url}`)
+})
